f4bcaee535edb86ff732534cbaaddb2c652cc3a2
JanReijer <117783889+JanReijer@users.noreply.github.com>
2026-03-24 00:28:36 -0400

    Add files via upload

--- 2D_Game/Transition/Fade.gd
+++ 2D_Game/Transition/Fade.gd
@@ -3,23 +3,24 @@ extends CanvasLayer
 @onready var color_rect: ColorRect = $ColorRect
 @onready var timer: Timer = $Timer
 
+# each level sets this to tell the fade where to go next
+var next_scene: String = ""
+
 func _ready() -> void:
 	color_rect.modulate.a = 0.0
 
 func _on_timeout() -> void:
 	print("Switching scene")
-	get_tree().change_scene_to_file("res://Story/story_scene_2.tscn")
+	get_tree().change_scene_to_file(next_scene)
 
-# troubleshooting
-func start_transition():
+func start_transition(target_scene: String) -> void:
 	print("Transition")
+	next_scene = target_scene
 
-	# calling audio to lower it as scene end
 	var music = get_parent().get_node("AudioStreamPlayer2D")
 
 	var tween = create_tween()
 	tween.set_parallel(true)
-	# setting volume -40 is practicaly silent
 	tween.tween_property(color_rect, "modulate:a", 1.0, 2.0)
 	tween.tween_property(music, "volume_db", -40.0, 2.0)
 
