cab43e863b99f4c68544c464c7f01cc748862890
JanReijer <117783889+JanReijer@users.noreply.github.com>
2026-03-24 00:24:37 -0400

    Add files via upload

---
+++ WinScreen/win_screen.gd
@@ -0,0 +1,8 @@
+extends Control
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Controls/controls.tscn")
+
+
+func _on_button_2_pressed() -> void:
+	pass # Replace with function body.
