6aac6417d96093be7affcf9363b581ba235f7cea
Gabriel De Araujo <123610934+Gabe3231@users.noreply.github.com>
2026-03-27 12:29:21 -0400

    Rule Update controls.gd

--- 2D_Project_v2/Controls/controls.gd
+++ 2D_Project_v2/Controls/controls.gd
@@ -8,14 +8,14 @@ func _ready() -> void:
 	label.bbcode_enabled = true
 	#various text and text settings like color, font and size
 	label.text = "[color=red][center][b][font_size=80]SPACE DELIVERY[/font_size][/b][/center][/color]\n\n"
-	label.text += "[color=red][font_size=40][b]Controls:[/b][/font_size][/color]\n"
+	label.text += "[color=red][font_size=40][b]Controls / Rules:[/b][/font_size][/color]\n"
 	label.text += "[color=red][font_size=40]Up Arrow = Move Up [/font_size][/color]\n"
 	label.text += "[color=red][font_size=40]Right Arrow = Move Right [/font_size][/color]\n"
 	label.text += "[color=red][font_size=40]Left Arrow = Move Left [/font_size][/color]\n"
 	label.text += "[color=red][font_size=40]Down Arrow = Move Down [/font_size][/color]\n"
 	label.text += "[color=red][font_size=40]Space or Mouse = Shoot [/font_size][/color]\n"
-	label.text += "[font_size=30]Enter - Start Game[/font_size]\n\n"
-	label.text += "[font_size=30][color=red]Press Play Begin[/color][/font_size]"
+	label.text += "[color=red][font_size=60]Avoid All Obstacles [/font_size][/color]\n"
+	label.text += "[font_size=30][color=red]Press Play To Begin[/color][/font_size]"
 
 #next scene
 func _on_button_pressed() -> void:
