8994c430e6f2aa4fbfde8e1a5de9f56347e7b15f
Gabriel <dearaujo.c.gabriel@gmail.com>
2026-04-22 14:43:16 -0400

    Enemy Facing and Animation Work

--- 3D_Game/enemy.gd
+++ 3D_Game/enemy.gd
@@ -1,28 +1,67 @@
 extends CharacterBody3D
 
-const SPEED := 5.0
+const WALK_SPEED := 4.0
+const RUN_SPEED := 6.0
+const ATTACK_RANGE := 2.0
+const RUN_RANGE := 6.0
+const CHASE_RANGE := 20.0
+
 @export var player_path: NodePath
+
 var player: Node3D = null
 
-func _ready() -> void:
-	player = get_node(player_path) as Node3D
+@onready var anim_player: AnimationPlayer = $AnimationPlayer
+@onready var anim_tree: AnimationTree = $AnimationTree
 
-func _physics_process(delta: float) -> void:
+func _ready() -> void:
+	player = get_node_or_null(player_path) as Node3D
+	print("player = ", player)
+	print(anim_player.get_animation_list())
+
+	anim_tree.active = false
+
+func _physics_process(_delta: float) -> void:
 	if player == null:
 		return
 
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	var dir = player.global_position - global_position
+	dir.y = 0
+	var dist = dir.length()
 
-	var direction = player.global_position - global_position
-	direction.y = 0
+	if dist <= ATTACK_RANGE:
+		play_anim("Attack")
+		velocity.x = 0.0
+		velocity.z = 0.0
+		face_player()
 
-	if direction.length() > 0.1:
-		direction = direction.normalized()
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+	elif dist <= RUN_RANGE:
+		play_anim("running")
+		dir = dir.normalized()
+		velocity.x = dir.x * RUN_SPEED
+		velocity.z = dir.z * RUN_SPEED
+		face_player()
+
+	elif dist <= CHASE_RANGE:
+		play_anim("walking")
+		dir = dir.normalized()
+		velocity.x = dir.x * WALK_SPEED
+		velocity.z = dir.z * WALK_SPEED
+		face_player()
+
 	else:
-		velocity.x = 0
-		velocity.z = 0
+		play_anim("idle")
+		velocity.x = 0.0
+		velocity.z = 0.0
 
+	velocity.y = 0.0
 	move_and_slide()
+
+func face_player() -> void:
+	var target = player.global_position
+	target.y = global_position.y
+	look_at(target, Vector3.UP)
+
+func play_anim(name: String) -> void:
+	if anim_player.has_animation(name):
+		if anim_player.current_animation != name:
+			anim_player.play(name)
