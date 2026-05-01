08386b2e868fb5cb9aa7594f8f3f21a763516455
Gabriel <dearaujo.c.gabriel@gmail.com>
2026-04-30 23:31:43 -0400

    Formatting text and comments

--- 3D_Game/controls/control_menu.gd
+++ 3D_Game/controls/control_menu.gd
@@ -1,5 +1,22 @@
 extends Node2D
 
+@onready var label: RichTextLabel = $RichTextLabel
+
 # basic connection to next scene when clicked
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://menu/main_menu.tscn")
+
+# formatting text better
+func _ready():
+	label.bbcode_enabled = true
+	label.text = "[center][b][font_size=36]WASD (to move)
+
+AND
+
+ARROW KEYS (to move)
+
+SHIFT TO SPRINT
+
+SPACE TO JUMP
+
+F to use flashlight[/font_size][/b][/center]"
--- 3D_Game/storyScene/story_start_scene.gd
+++ 3D_Game/storyScene/story_start_scene.gd
@@ -8,9 +8,19 @@ extends Node
 @export var fadeTime := 1.5
 @onready var fadeRect: ColorRect = $CanvasLayer/Fade
 @onready var timer: Timer = $Timer
+@onready var label: RichTextLabel = $CanvasLayer/RichTextLabel
 
-
+# formatting story text
 func _ready():
+	label.bbcode_enabled = true
+	label.text = "[center]\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n[b][font_size=36]Footage was recovered from a camera that had been lost in the subliminal space. It is unknown where this survivor is or their status. 
+
+This footage serves as training for any personnel entering the maze and its subsequent sections. 
+
+This footage serves as a warning to anyone who enters: creatures lurk within the maze.
+
+Use this footage to navigate the maze, but be wary at all times.[/font_size][/b][/center]"
+
 	# make mouse visable so can click
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	# fade setting
@@ -38,3 +48,4 @@ func fade_to_game():
 #next scene
 func change_to_game():
 	get_tree().change_scene_to_file(gameScenePath)
+	
