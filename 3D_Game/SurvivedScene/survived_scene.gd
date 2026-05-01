f6f3a0cfb6a0509932135be2da863ce74e9e519a
Gabriel <dearaujo.c.gabriel@gmail.com>
2026-04-30 23:43:07 -0400

    Formatting text and comments again

--- 3D_Game/SurvivedScene/survived_scene.gd
+++ 3D_Game/SurvivedScene/survived_scene.gd
@@ -1,8 +1,15 @@
 extends Node2D
 
+@onready var label: RichTextLabel = $Manager/RichTextLabel
+
+
 # had mouse issue where i could not click out of game
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	# text formatting
+	label.bbcode_enabled = true
+	label.text = "[center]\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n[b][font_size=36]This is where the footage stops. No other footage was recovered beyond this point. 
+	However, it is likely that there are other tapes scattered throughout other sections. [/font_size][/b][/center]"
 
 func _on_play_again_pressed() -> void:
 	get_tree().change_scene_to_file("res://storyScene/story_start_scene.tscn")
