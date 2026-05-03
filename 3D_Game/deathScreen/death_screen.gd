83cc984f567fdd623b8db057944c8b2bee238d62
JanReijer <117783889+JanReijer@users.noreply.github.com>
2026-05-03 02:46:59 -0400

    Added lore, and polished, and background

--- 3D_Game/deathScreen/death_screen.gd
+++ 3D_Game/deathScreen/death_screen.gd
@@ -1,5 +1,15 @@
 extends Node2D
 
+@onready var label: RichTextLabel = $RichTextLabel
+
+func _ready():
+	label.bbcode_enabled = true
+	label.text = "[center][outline_size=2][outline_color=black][b]\n\n\n[font_size=36]The footage has been [color=red]CORRUPTED[/color].
+
+Whatever was in that maze with you...
+
+[color=red]it found you first.[/color][/font_size][/b][/outline_color][/outline_size][/center]"
+
 # basic connection to next scene when clicked
 func _on_retry_button_down() -> void:
 	get_tree().change_scene_to_file("res://level.tscn")
