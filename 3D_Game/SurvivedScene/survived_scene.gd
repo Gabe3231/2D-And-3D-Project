fd9e8b96680668a67688e02eccc00df03f479687
JanReijer <117783889+JanReijer@users.noreply.github.com>
2026-05-03 02:47:28 -0400

    Reformatted lore, and polished scene

--- 3D_Game/SurvivedScene/survived_scene.gd
+++ 3D_Game/SurvivedScene/survived_scene.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@onready var label: RichTextLabel = $Manager/RichTextLabel
+@onready var label: RichTextLabel = $RichTextLabel
 
 
 # had mouse issue where i could not click out of game
@@ -8,8 +8,15 @@ func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	# text formatting
 	label.bbcode_enabled = true
-	label.text = "[center]\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n[b][font_size=36]This is where the footage stops. No other footage was recovered beyond this point. 
-	However, it is likely that there are other tapes scattered throughout other sections. [/font_size][/b][/center]"
+	label.text = "[center]\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n[outline_size=2][outline_color=black][b][font_size=36]This is where the footage stops.
+
+[color=red]No other footage was recovered[/color] beyond this point.
+
+It is likely that there are [color=red]other tapes[/color] scattered throughout other sections.
+
+Whatever you encountered in there...
+
+[color=red]it is still inside.[/color][/font_size][/b][/outline_color][/outline_size][/center]"
 
 func _on_play_again_pressed() -> void:
 	get_tree().change_scene_to_file("res://storyScene/story_start_scene.tscn")
